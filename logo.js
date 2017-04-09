c497cf194fedb0ce0e93cf4f08faeddafe38caf5
unknowndomain <me@unknowndomain.co.uk>
2017-04-09 17:48:24 +0100

    Fixing scaling

--- logo.js
+++ logo.js
@@ -1,9 +1,17 @@
 var points = Array( 30 );
 var triangleLayer;
+var thickness = 1;
+var speed = 2;
 
 window.onload = function() {
-	paper.setup( document.getElementById( 'logo' ) );
+	var logo = document.getElementById( 'logo' );
+	paper.setup( logo );
 	paper.install( window );
+
+	// Draw outline circle
+	var circle = new Path.Circle( paper.view.center, paper.view.size.height * 0.49 );
+	circle.strokeColor = 'black';
+	circle.strokeWidth = thickness;
 
 	var shape = [
 		[ 75, 231 ],
@@ -21,11 +29,6 @@ window.onload = function() {
 		[ 75, 231 ]
 	];
 
-	// Draw outline circle
-	var circle = new Path.Circle( new Point( logo.width / 4, logo.height / 4 ), ( logo.width / 4 ) - 10 );
-	circle.strokeColor = 'black';
-	circle.strokeWidth = 2;
-
 	// Draw letter H
 	var path = new Path();
 	for ( var point in shape ) {
@@ -33,7 +36,12 @@ window.onload = function() {
 	}
 	path.closePath();
 	path.strokeColor = 'black';
-	path.strokeWidth = 2;
+	path.strokeWidth = thickness;
+	path.fitBounds( new Size( paper.view.size.width * 0.8, paper.view.size.height * 0.8 ) );
+	path.translate( new Point(
+		paper.view.center.x - path.bounds.center.x,
+		paper.view.center.y - path.bounds.center.y
+	) );
 
 	// Create points along the H
 	for ( i = points.length; i -- ; ) {
@@ -48,7 +56,7 @@ window.onload = function() {
 	view.onFrame = function() {
 		for ( var p = 0; p < points.length; p++ ) {
 			var offset = path.getLocationOf( points[p] ).offset;
-			offset += Math.random()/2;
+			offset += Math.random() / speed;
 			if ( offset > path.length ) offset = 0;
 			var curve = path.getLocationAt( offset );
 			points[p] = curve.point;
@@ -73,7 +81,7 @@ function DelaunayRender( points ) {
 	for ( i = triangles.length; i; ) {
 		var path = new Path();
 		path.strokeColor = 'rgba( 0, 0, 0, 0.1 )';
-		path.strokeWidth = 1;
+		path.strokeWidth = thickness;
 		--i; addVertexPath( path, vertices[triangles[i]] );
 		--i; addVertexPath( path, vertices[triangles[i]] );
 		--i; addVertexPath( path, vertices[triangles[i]] );
@@ -84,7 +92,7 @@ function DelaunayRender( points ) {
 function addVertexPath( path, vertex ) {
 	var point = new Point( vertex[0], vertex[1] );
 	path.add( point );
-	var circle = new Path.Circle( point, 2 );
+	var circle = new Path.Circle( point, thickness * 1.25 );
 	circle.fillColor = 'black';
 	triangleLayer.addChild( circle );
 }
